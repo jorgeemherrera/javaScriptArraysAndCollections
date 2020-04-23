8140f831d03389b28daccd84a1325fc963c5f48a
Jorge Eliecer <jorge.m.h@outlook.com>
2020-04-23 18:28:51 -0500

    using spread operator to Chart.js

--- js/page.js
+++ js/page.js
@@ -3,12 +3,34 @@
 // Charts
 let ctx = document.getElementById('monthlySales').getContext('2d');
 let pieCtx = document.getElementById('deptSales').getContext('2d');
+let yearlyLabel = document.getElementById('yearlyTotal');
 
 let monthlyLabels = Array.of('Oct','Nov','Dec', 'Jan');
 let monthlySales = Array.of(500,900,300,400);
 
 let deptSales = Array.of(12,9,3,2);
 let deptLabels = Array.of('Hiking', 'Running', 'Hunting', 'Soccer');
+
+function addYearlyTotal(a,b,c,d) {
+    return a+b+c+d;
+}
+
+let octNums = Array.of(1200,1000,9000,7896);
+let novNums = Array.of(1000,860,6530,1256);
+let decNums = Array.of(857,320,1023,987);
+let janNums = Array.of(1250,854,8795,675);
+
+let totalNums = Array.of(
+    addYearlyTotal(...octNums),
+    addYearlyTotal(...novNums),
+    addYearlyTotal(...decNums), 
+    addYearlyTotal(...janNums)
+    );
+alert(addYearlyTotal(...totalNums));
+
+
+let yearlyTotal = addYearlyTotal(...monthlySales);
+yearlyLabel.innerHTML = `$ ${yearlyTotal}`
 
 // Bar
 var monthlySalesChart = new Chart(ctx, {
