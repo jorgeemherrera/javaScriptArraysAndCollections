0fe17714e7e46aff333c62e22aeae73db4e4999a
Jorge Eliecer <jorge.m.h@outlook.com>
2020-04-25 15:29:35 -0500

    Introducing to sets, iterating sets, updating chart.js from set and for each loops

--- js/page.js
+++ js/page.js
@@ -5,13 +5,59 @@ let ctx = document.getElementById('monthlySales').getContext('2d');
 let pieCtx = document.getElementById('deptSales').getContext('2d');
 let yearlyLabel = document.getElementById('yearlyTotal');
 
-let monthlyLabels = Array.of('Oct','Nov','Dec', 'Jan');
-let monthlySales = Array.of(500,750,3000,400,6700);
+// let monthlyLabels = Array.of('Oct','Nov','Dec', 'Jan');
+// let monthlySales = Array.of(500,750,3000,400,6700);
 
 let deptSales = Array.of(12,9,3,2);
 let deptLabels = Array.of('Hiking', 'Running', 'Hunting', 'Soccer');
 
+let newAmount = document.getElementById('itemAmount');
+let newMonth = document.getElementById('monthId');
+
 let yearlyTotal = 0;
+
+// Adding and Removing Values from Sets
+
+const monthlySales = new Set();
+const monthlyLabels = new Set();
+
+function addSale() {
+    // Add to a Set
+    monthlySales.add(parseInt(newAmount.value));
+    monthlyLabels.add(newMonth.value);
+
+    // Iterating a Set
+    yearlyTotal = 0;
+
+     // loop update values
+     monthlySalesChart.data.datasets.forEach((dataset) =>{
+        dataset.data = [];
+    });
+
+    for (let amount of monthlySales) {
+        yearlyTotal = amount + yearlyTotal;
+        yearlyLabel.innerHTML = yearlyTotal;
+
+        // loop update values
+        monthlySalesChart.data.datasets.forEach((dataset) =>{
+            dataset.data.push(amount);
+        })
+    }
+
+    monthlySalesChart.data.labels = Array.from(monthlyLabels);
+
+    monthlySalesChart.update();
+}
+
+function deleteVal() {
+    monthlySales.forEach((value1, value2, monthlySales) => {
+        alert(value1);
+    })
+}
+
+function addTotal() {
+    
+}
 
 // Iterating through Array of montlySales and adding in forEach
 function addYearlyTotal(x) {
@@ -53,10 +99,10 @@ function resetTo0() {
 var monthlySalesChart = new Chart(ctx, {
   type: 'bar',
   data: {
-      labels: monthlyLabels,
+      labels: [],
       datasets: [{
           label: '# of Sales',
-          data: monthlySales,
+          data: [],
           backgroundColor: [
               'rgba(238, 184, 104, 1)',
               'rgba(75, 166, 223, 1)',
